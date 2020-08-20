ca371e72832ec1c6730203dba252af3ff1c0d9de
zarhkoh <zarhkoh@hotmail.fr>
2020-08-20 15:07:47 +0200

    routes case-sensitive disabled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DefaultUrlSerializer, UrlTree, UrlSerializer } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,18 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
 import { JwtModule } from '@auth0/angular-jwt';
 import { AdminRegisterComponent } from './components/admin-register/admin-register.component';
+
+
+export class LowerCaseUrlSerializer extends DefaultUrlSerializer {
+  parse(url: string): UrlTree {
+    // Optional Step: Do some stuff with the url if needed.
+
+    // If you lower it in the optional step
+    // you don't need to use "toLowerCase"
+    // when you pass it down to the next function
+    return super.parse(url.toLowerCase());
+  }
+}
 
 
 
@@ -46,7 +59,10 @@ import { AdminRegisterComponent } from './components/admin-register/admin-regist
     NgbModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: UrlSerializer,
+    useClass: LowerCaseUrlSerializer
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
