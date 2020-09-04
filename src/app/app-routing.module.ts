1e71405538dbb292fe1b09d6190b501d5040bc56
zarhkoh <zarhkoh@hotmail.fr>
2020-09-04 15:37:40 +0200

    disable register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { TimbreListComponent } from './components/timbre-list/timbre-list.compon
 import { LoginComponent } from './components/login/login.component';
 import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
 import { AuthGuardService as AuthGuard } from './services/auth-guard.service';
-import { AdminRegisterComponent } from './components/admin-register/admin-register.component';
+// import { AdminRegisterComponent } from './components/admin-register/admin-register.component';
 import { FourOFourComponent } from './components/four-o-four/four-o-four.component';
 
 const routes: Routes = [
@@ -13,7 +13,7 @@ const routes: Routes = [
   { path: 'timbres', component: TimbreListComponent },
   { path: 'admin', component: LoginComponent },
   { path: 'adminpanel', canActivate: [AuthGuard], component: AdminPanelComponent },
-  { path: 'register', component: AdminRegisterComponent },
+  // { path: 'register', component: AdminRegisterComponent },
   { path: '**', component: FourOFourComponent }
 ];
 @NgModule({
