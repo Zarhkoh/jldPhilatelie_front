a0db0ba3a850b65f110dd48f64ce80c9d16f59a0
zarhkoh <zarhkoh@hotmail.fr>
2020-10-28 10:27:39 +0100

    set dynamic title/desc

--- src/app/components/four-o-four/four-o-four.component.ts
+++ src/app/components/four-o-four/four-o-four.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-four-o-four',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FourOFourComponent implements OnInit {
 
-  constructor() { }
+  constructor(private title: Title, private meta: Meta) { }
 
   ngOnInit(): void {
+    this.title.setTitle('Jld-philatelie - Timbres francais jusqu\'a 80 % de remise.');
+    this.meta.updateTag({
+      name: 'description', content: 'Vendeur particulier de timbres français, je vends mes timbres jusqu\'à 80% de remise sur la côte Yvert et Tellier dans le seul but de m\'acheter d\'autres timbres pour étoffer ma collection.'
+    });
   }
 
 }
