2c917fd803e5566191c5296349f5ffbc86820881
Zarhkoh <zarhkoh@hotmail.fr>
2020-12-09 12:29:36 +0100

    set visitor IP to 0.0.0.0 if he blocked ip tracking

--- src/app/services/user-informations.service.ts
+++ src/app/services/user-informations.service.ts
@@ -1,31 +1,45 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CONFIG } from 'src/CONFIG';
+import { LogService } from './log.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserInformationsService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private logService: LogService) { }
   url = CONFIG.service_url;
 
 
   async getIPAddress() {
-    return await this.http.get('https://api.ipify.org/?format=json').toPromise();
+    try {
+      return await this.http.get('https://api.ipify.org/?format=json').toPromise();
+    } catch (error) {
+      if (error.status == 0) {
+        return { "ip": "0.0.0.0" };
+      }
+      else {
+        this.logService.error(error, "user-informations.service");
+      }
+    }
   }
 
   async addVisit(browser) {
     let ipJson;
-    await this.getIPAddress().then(r => {
-      ipJson = JSON.stringify(r);
-      const params = {
-        visitorBrowser: browser,
-        visitorIp: JSON.parse(ipJson).ip,
-        visitorLastVisit: new Date()
-      };
-      return this.http.post(this.url + "/newVisit", { params }).toPromise();
-    });
-
+    try {
+      await this.getIPAddress().then(r => {
+        ipJson = JSON.stringify(r);
+        const params = {
+          visitorBrowser: browser,
+          visitorIp: JSON.parse(ipJson).ip,
+          visitorLastVisit: new Date()
+        };
+        return this.http.post(this.url + "/newVisit", { params }).toPromise();
+      });
+    } catch (error) {
+      console.log("STATUS:" + error);
+      this.logService.error(error, "user-informations.service");
+    }
   }
 
   getTotalVisits() {
